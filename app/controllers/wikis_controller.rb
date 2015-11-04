a13dbc8b920a03b1e4382b14d7ca69b1c584182e
Natalie Kueneman <tabalie@gmail.com>
2015-11-04 13:21:51 -0700

    Blocipedia - Adding new Wiki and editing Wiki works.

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,38 @@
+class WikisController < ApplicationController
+  def index
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+    if @wiki.save
+      flash[:notice] = "Wiki was saved!"
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the the wiki. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wikis. Please try again."
+      render :edit
+    end
+  end
+end
