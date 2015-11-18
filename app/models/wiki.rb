8611522bd7531eefe349a231c48a8d465b82669d
Natalie Kueneman <tabalie@gmail.com>
2015-11-17 18:48:51 -0700

    Blocipedia 8 - Private Wikis - some more progress.

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -2,4 +2,8 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
 
   scope :visible_to, -> (user) { user ? all : where(public: true) }
+
+  def public?
+    !self.public
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20151107192339) do
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.boolean  "private"
+    t.boolean  "public"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
